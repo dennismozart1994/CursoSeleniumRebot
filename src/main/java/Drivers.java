3535562b78deb2cc590328d5654cb32c6fa401e7
dennismozart1994 <dennis.mozart@gmail.com>
2018-08-03 18:15:44 -0300

    curso selenium

--- src/main/java/Drivers.java
+++ src/main/java/Drivers.java
@@ -11,19 +11,19 @@ public class Drivers {
 		{
 			case "Chrome": 
 				// Seta qual navegador o sistema deve abrir
-				System.setProperty("webdriver.chrome.driver", "C:\\Users\\INMETRICS\\Downloads\\drivers\\chromedriver.exe");
+				System.setProperty("webdriver.chrome.driver", "C:\\Users\\Dennis\\eclipse-workspace\\CursoSeleniumRebot\\drivers\\chromedriver.exe");
 				// Cria um webdriver do tipo correto
 				driver = new ChromeDriver();
 				// maximiza a janela
 				driver.manage().window().maximize();
 				break;
 			case "Firefox":
-				System.setProperty("webdriver.gecko.driver", "C:\\Users\\INMETRICS\\Downloads\\drivers\\geckodriver.exe");
+				System.setProperty("webdriver.gecko.driver", "C:\\Users\\Dennis\\eclipse-workspace\\CursoSeleniumRebot\\drivers\\geckodriver.exe");
 				driver = new FirefoxDriver();
 				driver.manage().window().maximize();
 				break;
 			default:
-				System.setProperty("webdriver.ie.driver", "C:\\Users\\INMETRICS\\Downloads\\drivers\\IEDriverServer.exe");
+				System.setProperty("webdriver.ie.driver", "C:\\Users\\Dennis\\eclipse-workspace\\CursoSeleniumRebot\\drivers\\IEDriverServer.exe");
 				driver = new InternetExplorerDriver();
 				driver.manage().window().maximize();
 				break;
